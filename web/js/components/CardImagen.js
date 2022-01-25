0c86188cbd696507a791d54bcdf0747c6ef3d0e5
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-25 20:26:03 +0100

    changed costructor of CardImagen.js

--- web/js/components/CardImagen.js
+++ web/js/components/CardImagen.js
@@ -1,12 +1,10 @@
 import Component from "./Component.js";
 
 export default class CardImagen extends Component {
-  constructor(elementoPadre, classNombre, src, alt) {
+  constructor(elementoPadre, classNombre, personaje) {
     super(elementoPadre, classNombre, "img");
-    this.src = src;
-    this.alt = alt;
 
-    this.elemento.src = this.src;
-    this.elemento.alt = this.alt;
+    this.elemento.src = personaje.img;
+    this.elemento.alt = `${personaje.nombre} ${personaje.familia}`;
   }
 }
--- web/js/index.js
+++ web/js/index.js
@@ -1,7 +1,18 @@
 import Component from "./components/Component.js";
 import personajes from "./personajes/personajes.js";
 import CardImagen from "./components/CardImagen.js";
+import CardCuerpo from "./components/CardCuerpo.js";
 
-const cardContenedor = document.querySelector(".characters-list");
+const cardsContenedor = document.querySelector(".characters-list");
 
-console.log(personajes);
+personajes.map((personaje) => {
+  const cardContenedor = new Component(cardsContenedor, "character col", "li");
+
+  const card = new Component(
+    cardContenedor.elemento,
+    "card character__card",
+    "div"
+  );
+
+  new CardImagen(card, "character__picture card-img-to", personaje);
+});
